ce6cd9543e0b0613fca7ad791714a25b1b0f261f
diaojianda <diaojianda@outlook.com>
2024-03-19 21:32:41 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM flink:1.18.1-scala_2.12-java11
+RUN mkdir -p $FLINK_HOME/usrlib
+COPY target/blog-flink-cdc-kubernetes-application-mode-demo-1.0-SNAPSHOT.jar $FLINK_HOME/usrlib/demo.jar
---
+++ src/main/java/org/example/DataStreamJob.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.TableDescriptor;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+/**
+ * Skeleton for a Flink DataStream Job.
+ *
+ * <p>For a tutorial how to write a Flink application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class DataStreamJob {
+
+    public static void main(String[] args) throws Exception {
+        // Sets up the execution environment, which is the main entry point
+        // to building Flink applications.
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        // mysql props
+        String mysqlHost = System.getProperty("mysql.hostname");
+        String mysqlPort = System.getProperty("mysql.port");
+        // 表结构
+        Schema schema = Schema.newBuilder()
+                .column("id", DataTypes.INT().notNull())
+                .column("name", DataTypes.STRING())
+                .column("create_time", DataTypes.TIMESTAMP())
+                .primaryKey("id")
+                .build();
+        TableDescriptor sourceTableDesc = TableDescriptor.forConnector("mysql-cdc")
+                .option("hostname", mysqlHost)
+                .option("port", mysqlPort)
+                .option("username", "root")
+                .option("password", "000000")
+                .option("database-name", "playground")
+                .option("table-name", "cdc_table")
+                .option("server-time-zone", "UTC")
+                .option("scan.startup.mode", "earliest-offset")
+                .schema(schema)
+                .build();
+        TableDescriptor sinkTableDesc = TableDescriptor.forConnector("jdbc")
+                .option("url", "jdbc:mysql://" + mysqlHost + ":" + mysqlPort + "/playground")
+                .option("username", "root")
+                .option("password", "000000")
+                .option("table-name", "cdc_table_copy")
+                .schema(schema)
+                .build();
+        tableEnv.createTable("source", sourceTableDesc);
+        tableEnv.createTable("sink", sinkTableDesc);
+
+        tableEnv.executeSql("insert into sink select * from source");
+
+        env.execute("Flink CDC K8s DEMO");
+    }
+}
